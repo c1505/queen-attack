2f8bc3d42b1765d4eb6cdfb13d2f9a366b3d1cdb
c1505 <coreym247@gmail.com>
2016-08-25 19:21:03 +0000

    wip extracting queen object

--- queen_attack.rb
+++ queen_attack.rb
@@ -1,15 +1,59 @@
-# do i want a board object?
+
 require 'pry'
-class Queens
-  attr_reader :white, :black
+# test is looking for a position when calling Queens.black so it isn't really practical to make each Queen objects
+class Queen 
+
   def initialize(args={})
-    @white = args[:white] || [0, 3]
-    @black = args[:black] || [7, 3]
+    # @white = args[:white] || [0, 3]
+    # @black = args[:black] || [7, 3]
+    @color = color(args)
+    # if args[:white]
+    #   @position = 
+    # @position = position
+  end
+  
+  def color(args)
+    if args[:white]
+      'W'
+    elsif args[:black]
+      'B'
+    end
+  end
+end
+    
+class Queens
+  # attr_reader :white, :black
+  attr_writer :board
+  def initialize(args={})
+    
+    if args[:white]
+      @white = Queen.new(args)
+    else
+      args[:white] = [0, 3]
+      @white = Queen.new(args)
+    end
+    
+    if args[:black]
+      @black = Queen.new(args)
+    else
+      args[:black] = [7, 3]
+      @black = Queen.new(args)
+    end
+    # @white = args[:white] || [0, 3]
+    # @black = args[:black] || [7, 3]
     raise ArgumentError, 'Queens cannot occupy the same space' if @white == @black
   end
   
-  def to_s
-    board = <<-BOARD.chomp
+  def white
+    @white.position
+  end
+  
+  def black
+    @black.position
+  end
+  
+  def board
+    @board ||= <<-BOARD.chomp
 _ _ _ _ _ _ _ _
 _ _ _ _ _ _ _ _
 _ _ _ _ _ _ _ _
@@ -19,16 +63,29 @@ _ _ _ _ _ _ _ _
 _ _ _ _ _ _ _ _
 _ _ _ _ _ _ _ _
     BOARD
-    a = board.lines
-    vertical = @white[0]
-    horizontal = @white[1] * 2
-    a[vertical][horizontal] = 'W'
+  end
+  
+  def to_s
+    board_rows = board.lines
     
-    vertical = @black[0]
-    horizontal = @black[1] * 2
-    a[vertical][horizontal] = 'B'
+    add_to_board(@white, 'W', board_rows)
+    add_to_board(@black, 'B', board_rows)
+    # vertical = @white[0]
+    # horizontal = @white[1] * 2
+    # board_rows[vertical][horizontal] = 'W'
     
-    a.join
+    # vertical = @black[0]
+    # horizontal = @black[1] * 2
+    # board_rows[vertical][horizontal] = 'B'
+    
+    board_rows.join
+  end
+  
+  def add_to_board(color, character, board_rows)
+    # board_rows = board.lines
+    vertical = color[0]
+    horizontal = color[1] * 2
+    board_rows[vertical][horizontal] = character
   end
   
   def attack?
@@ -64,5 +121,5 @@ _ _ _ _ _ _ _ _
 end
 # 
 
-class Board
-end
+# class Board
+# end
