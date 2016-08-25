ddfdc2f69a29f7ee9c5745314498b43d6a4c6dbd
c1505 <coreym247@gmail.com>
2016-08-25 19:33:40 +0000

    extracted queen back to working

--- queen_attack.rb
+++ queen_attack.rb
@@ -2,23 +2,12 @@
 require 'pry'
 # test is looking for a position when calling Queens.black so it isn't really practical to make each Queen objects
 class Queen 
-
-  def initialize(args={})
-    # @white = args[:white] || [0, 3]
-    # @black = args[:black] || [7, 3]
-    @color = color(args)
-    # if args[:white]
-    #   @position = 
-    # @position = position
+  attr_reader :position, :color
+  def initialize(color, position)
+    @color = color
+    @position = position
   end
   
-  def color(args)
-    if args[:white]
-      'W'
-    elsif args[:black]
-      'B'
-    end
-  end
 end
     
 class Queens
@@ -27,21 +16,21 @@ class Queens
   def initialize(args={})
     
     if args[:white]
-      @white = Queen.new(args)
+      @white = Queen.new('W', args[:white])
     else
       args[:white] = [0, 3]
-      @white = Queen.new(args)
+      @white = Queen.new('W', args[:white])
     end
     
     if args[:black]
-      @black = Queen.new(args)
+      @black = Queen.new('B', args[:black])
     else
       args[:black] = [7, 3]
-      @black = Queen.new(args)
+      @black = Queen.new('B', args[:black])
     end
     # @white = args[:white] || [0, 3]
     # @black = args[:black] || [7, 3]
-    raise ArgumentError, 'Queens cannot occupy the same space' if @white == @black
+    raise ArgumentError, 'Queens cannot occupy the same space' if @white.position == @black.position
   end
   
   def white
@@ -68,8 +57,8 @@ _ _ _ _ _ _ _ _
   def to_s
     board_rows = board.lines
     
-    add_to_board(@white, 'W', board_rows)
-    add_to_board(@black, 'B', board_rows)
+    add_to_board(@white, board_rows)
+    add_to_board(@black, board_rows)
     # vertical = @white[0]
     # horizontal = @white[1] * 2
     # board_rows[vertical][horizontal] = 'W'
@@ -81,15 +70,15 @@ _ _ _ _ _ _ _ _
     board_rows.join
   end
   
-  def add_to_board(color, character, board_rows)
+  def add_to_board(queen, board_rows)
     # board_rows = board.lines
-    vertical = color[0]
-    horizontal = color[1] * 2
-    board_rows[vertical][horizontal] = character
+    vertical = queen.position[0]
+    horizontal = queen.position[1] * 2
+    board_rows[vertical][horizontal] = queen.color
   end
   
   def attack?
-    if @black[0] == @white[0] || @black[1] == @white[1]
+    if @black.position[0] == @white.position[0] || @black.position[1] == @white.position[1]
       true
     elsif positive_diagonal? || other_diagonal?
       true
@@ -100,22 +89,22 @@ _ _ _ _ _ _ _ _
   
   def positive_diagonal?
     diagonal_array = []
-    wh = @white
+    wh = @white.position
     while wh[0] < 7 && wh[1] < 7
       wh = wh.map {|f| f + 1 }
       diagonal_array << wh
     end
-    diagonal_array.include?(@black)
+    diagonal_array.include?(@black.position)
   end
   
   def other_diagonal?
     diagonal_array = []
-    wh = @white
+    wh = @white.position
     while wh[0] < 7 && wh[1] < 7
       wh = [wh[0] - 1, wh[1] + 1]
       diagonal_array << wh
     end
-    diagonal_array.include?(@black)
+    diagonal_array.include?(@black.position)
   end
 
 end
